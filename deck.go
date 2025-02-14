fc7f317820cc1b9ed870d9a78595bb73232d43c7
anukulpr1me <anukul_chandra@outlook.com>
2025-02-14 02:17:28 -0800

    added docker file

--- deck.go
+++ deck.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"os"
 	"strings"
 )
 
@@ -35,6 +37,24 @@ func newDeck() deck {
 	}
 	return cards
 }
-func (d deck) saveToFile(fileName string) error{
+func (d deck) saveToFile(fileName string) error {
 	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), "," )
+	return deck(s)
+}
+
+
+func (d deck) shuffle() {
+	for i := range d{
+		newPosition := rand.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
